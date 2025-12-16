12a92bb44c7ef8e4583436551c1664ce0d718dbb
https://github.com/jd205/learning_react.git <medinajd205@gmail.com>
2025-12-15 19:37:15 -0500

    first commit22

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: "/Create Account Form",
+    base: "/paolaweb/",
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
